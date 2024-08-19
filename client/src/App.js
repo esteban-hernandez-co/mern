18da4df79d7d506e2d2b7f306ee56f178073df77
Esteban Hernandez <esteban.hernandez@nexted.com.au>
2024-08-19 17:10:11 +1000

    Using bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 import UsersList from './UsersList';
 import EditUser from './EditUser';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import AddUser from './AddUser';
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <h1>CRUD MERN stack</h1>
+      
 
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <a className="navbar-brand" href="#">navbar</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="addUser">Add User</a>
+              </li>
+              
+              
+            </ul>
+            
+          </div>
+        </div>
+      </nav>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<UsersList/>} exact></Route>
