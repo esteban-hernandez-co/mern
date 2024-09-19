a06e9ee16c0d1b84e5bc51e253aafe8072e51699
Esteban Hernandez <esteban.hernandez@nexted.com.au>
2024-09-19 15:15:42 +1000

    change path to users components

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import UsersList from './UsersList';
-import EditUser from './EditUser';
-import AddUser from './AddUser';
-import Dashboard from './Dashboard'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-
+import UsersList from './components/user/UsersList';
+import EditUser from './components/user/EditUser';
+import AddUser from './components/user/AddUser';
+import Dashboard from './Dashboard';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-
-      
-      
       <BrowserRouter>
         <Routes>
-          <Route path='/user' element={<UsersList/>} exact></Route>
-          <Route path='/user/add' element={<AddUser/>} exact></Route>
-          <Route path='/user/edit/:_id' element={<EditUser/>} exact></Route>
-          <Route path='/dashboard' element={<Dashboard/>} exact></Route>
-          <Route path='/' element={<Dashboard/>} exact></Route>
-          <Route path='/settings' element={<Dashboard/>} exact></Route>
-          <Route path='/profile' element={<Dashboard/>} exact></Route>
-          <Route path='/login' element={<Dashboard/>} exact></Route>
-          <Route path='/logout' element={<Dashboard/>} exact></Route>
-          
-
+          <Route path="/user" element={<UsersList />} exact></Route>
+          <Route path="/user/add" element={<AddUser />} exact></Route>
+          <Route path="/user/edit/:_id" element={<EditUser />} exact></Route>
+          <Route path="/dashboard" element={<Dashboard />} exact></Route>
+          <Route path="/" element={<Dashboard />} exact></Route>
+          <Route path="/settings" element={<Dashboard />} exact></Route>
+          <Route path="/profile" element={<Dashboard />} exact></Route>
+          <Route path="/login" element={<Dashboard />} exact></Route>
+          <Route path="/logout" element={<Dashboard />} exact></Route>
         </Routes>
       </BrowserRouter>
-      
-      
-      
     </div>
   );
 }
